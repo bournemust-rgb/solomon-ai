e94e76f7174766b21a0fd269ea3107529667e927
Ridhor hendricks <bournemust@gmail.com>
2026-07-11 16:38:05 +0200

    Changed option 3 to 'Query example'

--- menu.js
+++ menu.js
@@ -14,7 +14,7 @@
     "contact":"060 507 4461 | Office: "+OFFICE_NUMBER+" | Email: "+OFFICE_EMAIL,
     "1":"PRICING (excl VAT)\nRims: R1000-R1500/set\nSheet: R175-R350/sqm\nCoating: R16/kg B/W, R17-R20/kg premium\nBlasting: R8-R12/kg\nTruck: R5000-R7500\nMin: R173.99\n\nType *menu* to go back.",
     "2":"COLOURS\nStandard: Black, White, Brown, Bronze, Charcoal: R175-R250/sqm\nHammered: R225+\nMetallic/Custom/RAL: R300+\n\nType *menu* to go back.",
-    "3":"Send: quote 20kg gate charcoal | quote 4 rims metallic | quote 10sqm sheet black | quote 20kg blasting only\n\nType *menu* to go back.",
+    "3":"Query example: quote 20kg gate charcoal | quote 4 rims metallic | quote 10sqm sheet black | quote 20kg blasting only\n\nType *menu* to go back.",
     "4":"TURNAROUND\nUnder 1 ton: 3 working days. Over 1 ton: 5-8 working days.\n\nType *menu* to go back.",
     "5":"BUSINESS HOURS\nMon-Thurs: 8AM-4:45PM\nFri: 8AM-2:45PM\nClosed weekends.\n\nType *menu* to go back.",
     "6":"DELIVERY & COLLECTION\nR150 metro. Free collection. Collect within 7 days. Late: 7% daily storage.\n\nType *menu* to go back.",
@@ -31,3 +31,4 @@
   };
 }
 module.exports = { buildMenu };
+
